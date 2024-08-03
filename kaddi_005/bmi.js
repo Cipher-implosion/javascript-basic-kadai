fe7f85db4f0ad157b98304878baea75c4d1c84cd
Cipher-implosion <bua80vo@gmail.com>
2024-08-03 15:52:43 +0900

    kaddi_005を追加

---
+++ kaddi_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の代入
+// 体重の代入
+let weight = 68;
+// 体重 の出力
+console.log(weight);
+// 身長の代入
+let height =1.7;
+// 身長 の出力
+console.log(height);
+
+// bmiの計算
+let bmi = weight / (height * height);
+// bmi の出力
+console.log(bmi);
